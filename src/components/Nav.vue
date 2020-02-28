efde44ed77e2ead886ee680f9b534d9bc32695c3
dashixiong <2788347834@qq.com>
2020-02-28 23:16:30 +0800

    开始memo记账本项目

---
+++ src/components/Nav.vue
@@ -0,0 +1,17 @@
+<template>
+   <div>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/labels">Labels</router-link>
+   </div>
+   
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Nav'
+    };
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/labels.vue
@@ -0,0 +1,16 @@
+<template>
+   <div>
+      123
+   </div>
+   
+</template>
+
+<script lang="ts">
+    export default {
+        name: ''
+    };
+</script>
+
+<style scoped lang="scss">
+
+</style>
